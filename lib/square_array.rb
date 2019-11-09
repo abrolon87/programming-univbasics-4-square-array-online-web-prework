9ed25bcb815dabe7319b25a48f4e63ee637a70fe
Amanda R <abrolon87@gmail.com>
2019-11-09 23:06:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,7 @@
 def square_array(array)
-numbers = [1,2,3]
+  while array do
+     **    
+  end
 
-square_array(numbers)
-# => [1,4,9])
 
-new_numbers = [9,10,16,25]
-
-square_array(new_numbers)
-# => [81,100,256,625]
 end
